3ff3a1c51638376b7cf6fcaed11f1fd0fbdafb28
Rafael Soto <rafael.soto@gmail.com>
2015-11-29 21:12:03 -0200

    JsonConvert Commit

---
+++ src/main/java/br/com/rafaelsoto/jxmind/JSONConverter.java
@@ -0,0 +1,41 @@
+package br.com.rafaelsoto.jxmind;
+
+import java.lang.reflect.Field;
+
+import com.google.gson.FieldNamingStrategy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JSONConverter {
+
+	public static void main(String[] args) throws JxmindException {
+		
+		ParseXmind parseXmind = new ParseXmind("src/main/resources/content.xml");
+		XmindNode node = parseXmind.parseXmindFileDom4J();
+		
+		GsonBuilder builder = new GsonBuilder();
+		
+		builder.setFieldNamingStrategy(new FieldNamingStrategy() {
+			
+			public String translateName(Field field) {
+				
+				if(field.getName().equalsIgnoreCase("title"))
+				{
+					return "name";
+				}
+				else if(field.getName().equalsIgnoreCase("childNodes"))
+				{
+					return "children";
+				}
+				else
+					return field.getName();
+			}
+		});
+		
+        Gson gson = builder.setPrettyPrinting().create();
+        System.out.println(gson.toJson(node));
+		
+	}
+	
+	
+}
